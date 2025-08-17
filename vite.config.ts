d206cb3bce322972a4379a0cc1afbb62cc81b7f1
edfaruq <14.alfaruq@gmail.com>
2025-08-17 18:27:10 +0700

    fix: update vite config and env for production

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
+    base: '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
